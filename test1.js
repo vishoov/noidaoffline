58b5183b57799f34864b1b1877088d8f7d7a2cbb
Vishoo <vverma971@gmail.com>
2025-06-12 23:18:05 +0530

    push

---
+++ test1.js
@@ -0,0 +1,14 @@
+const arr=[6, 3, 4, 2, 8, 5];
+
+arr.sort();
+console.log(arr);
+
+const name = "Acciojob";
+const age = 10;
+
+console.log(name, age)
+
+
+// int num = 10;
+// string name = "acciojob"
+

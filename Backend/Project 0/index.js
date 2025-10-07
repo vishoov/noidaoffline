4336c3c04911a20c5ea4277919cc4080e8f82967
Vishoo <vverma971@gmail.com>
2025-10-07 13:36:02 +0530

    backend

--- Backend/Project 0/index.js
+++ Backend/Project 0/index.js
@@ -8,11 +8,11 @@ const todoRoutes = require('./view/todo.view')
 const PORT = 3000;
 // http://localhost:3000/
 const dynamic = require('./view/dynamic.view')
+const morgan = require('morgan')
 
-
-app.use(express.json());
 // express.json -> request json -> parse 
 app.use(todoRoutes)
+app.use(morgan('combined'))
 
 //app -> is an instance of express application
 app.use(dynamic)
@@ -47,15 +47,73 @@ app.get('/home', (req, res)=>{
     //res -> response object -> contains information about the response sent back to the client
 })
 
+//07/10/2025
+//topic : middlewares
 
 
+const middlewarefunction = (req, res, next)=>{
 
+    console.log("This is the middleware that has completed execution")
+    next(); //next keyword tells express to continute with next function in line
+}
 
+app.use(middlewarefunction)
+
+const logs = [
+
+]
+
+const loggingMiddleware = (req, res, next)=>{
+    const method = req.method;
+    const path = req.path;
+    const time =  Date.now().toLocaleString();
+    const ip = req.ip;
+    const baseUrl = req.baseUrl
+    const log = {
+        method, 
+        path, 
+        time, 
+        ip,
+        baseUrl
+    }
+    console.log(log);
+    logs.push(log)
+    next();
+
+}
+
+app.use(loggingMiddleware)
 //i want to make a /about route with GET method when a user visits it says welcome to about page
 //i want to make a /profile route with GET method when a user visits it says welcome to profile page
 
 //i want to make a / route with GET method when a user visits it says welcome to root page
+app.get("/greet", (req, res)=>{
+    console.log("greet route called")
+    res.send("Welcome to our server")
+})
 
+
+
+app.use(express.json()) //this is a syntax for application level middleware
+
+const middlewareFunc = (req, res, next)=>{
+    console.log("Middleware called")
+    next();
+}
+
+app.get("/data", middlewareFunc, (req, res)=>{
+    const data = req.body;
+    console.log(data)
+
+    res.json({
+        data,
+        message:"data sent back"
+    })
+})
+
+
+
+//07/10/2025 ends here
 
 
 
